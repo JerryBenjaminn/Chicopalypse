74f9ee61acd3622c17d28e56b97af7a6b64a9275
Jerry Nordman <jerry.nordman@student.careeria.fi>
2021-09-18 21:41:27 +0300

    Added player movement and a placeholder background

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed; //Luodaan muuttuja, joka ohjaa pelaajan nopeutta. SerializeFieldiä käytetään, kun halutaan muuttujan olevan private, mutta näkyvän Unityn editorissa
+
+    [SerializeField] private Rigidbody2D rb; // Luodaan scriptiin Rididbodylle muuttuja, jotta sitä voidaan kutsua
+    private bool facingRight = true; // Luodaan boolean muuttuja, jotta pelaajan suunta voidaan varmentaa
+    [SerializeField] private float moveDirection; //Luodaan muuttuja, jotta voidaan luoda peliin controllit
+
+
+    private void Awake() //Poistetaan Start()-metodi ja luodaan sen tilanne Awake()-metodi. Awake metodia kutsutaan nopeammin, kuin Start-metodia ja se nopeuttaa pelin aloítusta
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); //Sijoitetaan  x-koordinaatin liikkuvuus tiettyyn näppäimeen
+        rb.velocity = new Vector2(moveDirection * movementSpeed, rb.velocity.y);
+    }
+}
